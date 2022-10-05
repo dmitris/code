2b992540bf42edad776e6ca5be916b26687c7b05
Dmitri S <dsavints@gmail.com>
2022-10-05 13:12:19 +0200

    ch7 fview variation

--- ch7/ch7-fview/src/main.rs
+++ ch7/ch7-fview/src/main.rs
@@ -1,29 +1,52 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::env;
+// fview program modified to print also the last line which may be < BYTES_PER_LINE.
 
-const BYTES_PER_LINE: usize = 16;                  // <1>
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+const BYTES_PER_LINE: usize = 16; // <1>
 
 fn main() {
-  let arg1 = env::args().nth(1);
+    let arg1 = env::args().nth(1);
 
-  let fname = arg1.expect("usage: fview FILENAME");
+    let fname = arg1.expect("usage: fview FILENAME");
 
-  let mut f = File::open(&fname).expect("Unable to open file.");
-  let mut pos = 0;
-  let mut buffer = [0; BYTES_PER_LINE];
+    let mut f = File::open(&fname).expect("Unable to open file.");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
 
-    while let Ok(_) = f.read_exact(&mut buffer) {
+    // possible after 1.65 / #![feature(let_chains)]?
+    // while let Ok(n) = f.read(&mut buffer) && n > 0 {
+
+    while let Ok(n) = f.read(&mut buffer) {
+        if n == 0 {
+            break;
+        }
         print!("[0x{:08x}] ", pos);
-        for byte in &buffer {
-            match *byte {
+        for b in buffer.iter().take(n) {
+            match b {
                 0x00 => print!(".  "),
                 0xff => print!("## "),
-                _ => print!("{:02x} ", byte),
+                _ => print!("{:02x} ", b),
             }
         }
 
-        println!("");
-        pos += BYTES_PER_LINE;
+        println!();
+        pos += n;
     }
 }
+
+//// original code
+//   while f.read_exact(&mut buffer).is_ok() {
+//     print!("[0x{:08x}] ", pos);
+//     for byte in &buffer {
+//       match *byte {
+//         0x00 => print!(".  "),
+//         0xff => print!("## "),
+//         _ => print!("{:02x} ", byte),
+//       }
+//     }
+
+//     println!();
+//     pos += BYTES_PER_LINE;
+//   }
